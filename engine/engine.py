26ff766ee0479357b52fc7f231ca4c85971405ad
TomfoolerousGitHub <jacob.heath2@student.education.wa.edu.au>
2024-03-12 11:54:01 +0800

    Added basic database initialisation

--- engine/engine.py
+++ engine/engine.py
@@ -15,6 +15,16 @@ class Engine:
         self.conn.autocommit = True
         self.cursor = self.conn.cursor()
 
-    def _run_sql(self, sql, values):
+    def _run_sql(self, sql, values) -> tuple:
         self.cursor.execute(sql, values)
         return self.cursor.fetchall()
+
+    def _initialise_db(self) -> bool:
+        self._run_sql("CREATE DATABASE IF NOT EXISTS %s", (os.getenv("DB_NAME"),))
+        self._run_sql("USE %s", (os.getenv("DB_NAME"),))
+        for i in range(0, 4):
+            self._run_sql(f"CREATE TABLE IF NOT EXISTS players_202{i} (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, cost INT, kicks INT, handballs INT, marks INT, tackles INT, freekicksfor INT, freekicksagainst INT, hitouts INT, goals INT, behinds INT, team TEXT)", ())
+            self._run_sql(f"CREATE TABLE IF NOT EXISTS matches_202{i} (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, home_team TEXT, away_team TEXT, ground_id INT, date DATE, time TIME, home_points INT, away_points INT, temperature INT, weather TEXT)", ())
+        self._run_sql("CREATE TABLE IF NOT EXISTS teams (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, colour TEXT, coach TEXT)", ())
+        self._run_sql("CREATE TABLE IF NOT EXISTS grounds (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, capacity INT)", ())
+        return True
