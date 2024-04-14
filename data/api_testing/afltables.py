97e9e770504c14d157daf05f9df5ead712ed4d9a
Danny Edwards <danny_edwards09@icloud.com>
2024-04-14 23:10:18 +0800

    fixed issue with round 1 bye appending before player name. Also started working on making it collect data for all years

--- data/api_testing/afltables.py
+++ data/api_testing/afltables.py
@@ -6,11 +6,6 @@ def get_html(url):
   response = requests.get(url)
   return response.text
 
-team = 'adelaide'
-year = '2023'
-
-soup = bs.BeautifulSoup(get_html(f'https://afltables.com/afl/stats/teams/{team}/{year}_gbg.html'), 'html.parser')
-
 
 def season_data(tables):
   headings = (tables.find("ul", {"class": "simpleTabsNavigation"})).find_all("li")
@@ -18,6 +13,11 @@ def season_data(tables):
 
   for i in range(len(headings)-1):
     headings_list.append(headings[i].a.text)
+  
+  if 'BR' in headings_list:
+    headings_list.remove('BR')
+  headings_list.remove('SU')
+
 
   data = tables.find_all("div", {"class": "simpleTabsContent"})
 
@@ -45,7 +45,8 @@ def season_data(tables):
     else:
       rounds_list.append(i)
 
-  return rounds_list, bye_rounds
+  return rounds_list, bye_rounds, num_rounds
+
 
 def key_to_shorthand(key):
   if key == 'Disposals':
@@ -76,8 +77,6 @@ def key_to_shorthand(key):
     return 'FF'
   elif key == 'Frees Against':
     return 'FA'
-  elif key == 'Brownlow Votes':
-    return 'BR'
   elif key == 'Contested Possessions':
     return 'CP'
   elif key == 'Uncontested Possessions':
@@ -99,32 +98,21 @@ def key_to_shorthand(key):
     exit()
 
 
-def get_player_data(tables, bye_rounds, year, num_rounds):    
+def get_player_data(tables, bye_rounds, year, num_rounds, final_data):    
   #returns data as lists of each data point which needs to be decoded into lists of each player
-  final_data = {
-    "2024": {
-      "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "BR": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
-    },
-    "2023": {
-      "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "BR": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
-    },
-    "2022": {
-      "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "BR": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
-    },
-    "2021": {
-      "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "BR": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
-    },
-    "2020": {
-      "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "BR": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
-    }
-}
 
   temp_player_data = []
   
   data = tables.find_all("div", {"class": "simpleTabsContent"})
+  data = data[0:-1]
 
-  # you can use data[x] to get the xth table of 
-  for i in data[0:-1]:        # -1 to remove the last table which is the subs
+  #removing brownlow votes table as it is not available for the current year
+  for i in data:
+    if i.table.thead.tr.th.text == 'Brownlow Votes':
+      data.remove(i)
+
+  # you can use data[n] to get the nth table of data 
+  for i in data:        # -1 to remove the last table which is the subs
     table_data = i.table.tbody
     table_key = i.table.thead.tr.th.text
 
@@ -134,8 +122,14 @@ def get_player_data(tables, bye_rounds, year, num_rounds):
         round_counter += 1
         
         if round_counter in bye_rounds:
-          temp_player_data.append(None)
-          round_counter += 1
+          if round_counter == 1:                    
+          # if it is the first round then the player name needs to be added to the list first and then the bye round
+            temp_player_data.append(cell.text)
+            temp_player_data.append(None)
+            continue                            #there it continues as there is no need to add the player_name later
+          else:
+            temp_player_data.append(None)
+            round_counter += 1
 
         if cell.text == '\xa0':
           temp_player_data.append(None)
@@ -145,22 +139,84 @@ def get_player_data(tables, bye_rounds, year, num_rounds):
           continue
 
         temp_player_data.append(cell.text)
+        # print(temp_player_data)
       
       # print(f'{table_key}: {temp_player_data} \n\n')
       final_data[year][key_to_shorthand(table_key)][temp_player_data[0]] = temp_player_data[1:]
-      temp_player_data = []
+      temp_player_data.clear()
   
   final_data[year]["number_rounds"] = num_rounds
-  # print(final_data['2023'])
-  
+  pprint(final_data)   
+
+  return final_data
 
 
+def format_data(unformatted_data):
+  formatted_data = []
+  formatted_player_data = []
+  name_element = []
+  match_element = []
 
-tables = soup.find("div", {"class": "simpleTabs"})
+  for player in unformatted_data["2023"]["DI"]:     
+    #start with disposals as it is the first key then it will link everything else in. Uses 2023 as that has latest list of players
+    
+    name_element.append(player)
+    for i in range(23):
+      name_element.append(None)
+    formatted_player_data.append(name_element)
+    
+    round_id_incrementor = 1
+    for year in unformatted_data:
+      round_id = f'{year}_{round_id_incrementor}'
+      for table_key in unformatted_data[year]:
+        match_element.append(round_id)
+        for data in unformatted_data[year]:
+          print(unformatted_data[year])
+          match_element.append(unformatted_data[year][data][player][round_id_incrementor])
+        
+        formatted_player_data.append(match_element)
+        match_element.clear()
+      round_id_incrementor += 1
+      print(formatted_player_data)
+      exit()
 
-rounds, bye_rounds = season_data(tables)
+        
 
-unformatted_data = get_player_data(tables, bye_rounds, year, num_rounds)
+unformatted_data = {
+  "2024": {
+    "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
+  },
+  "2023": {
+    "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
+  },
+  "2022": {
+    "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
+  },
+  "2021": {
+    "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
+  },
+  "2020": {
+    "DI": {}, "KI": {}, "MK": {}, "HB": {}, "GL": {}, "BH": {}, "HO": {}, "TK": {}, "RB": {}, "I5": {}, "CL": {}, "CG": {}, "FF": {}, "FA": {}, "CP": {}, "UP": {}, "CM": {}, "MI": {}, "OP": {}, "BO": {}, "GA": {}, "%P": {}
+  }
+}
+
+years = ['2024', '2023', '2022', '2021', '2020']
+
+
+team = 'adelaide'
+
+for year in years:
+  soup = bs.BeautifulSoup(get_html(f'https://afltables.com/afl/stats/teams/{team}/{year}_gbg.html'), 'html.parser')
+
+  tables = soup.find("div", {"class": "simpleTabs"})
+
+  rounds, bye_rounds, num_rounds = season_data(tables)
+
+  unformatted_data = get_player_data(tables, bye_rounds, year, num_rounds, unformatted_data)
+  # pprint(unformatted_data)
+  exit()
+
+format_data(unformatted_data)
 
 '''
 unformatted data:
